a174d4b93d313f285ee27316596873f02cf81d7e
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-27 11:10:04 +0200

    adjust input style

--- 20231025_rest_countries/src/components/Navigation/index.jsx
+++ 20231025_rest_countries/src/components/Navigation/index.jsx
@@ -4,9 +4,15 @@ export const Navigation = () => {
   return (
     <nav className="nav container-sm center" role="navigation">
       <div className="nav__wrapper">
-        <div className="nav__search">
-          <input type="text" placeholder="Search for a country..." />
-        </div>
+        <label className="nav__search" htmlFor="search">
+          <ion-icon className="nav__search-icon" name="search-sharp"></ion-icon>
+          <input
+            className="nav__search-input"
+            id="search"
+            type="search"
+            placeholder="Search for a country..."
+          />
+        </label>
         <div className="nav__filter">
           <select name="region" id="region">
             <option value="none">Filter By Region</option>
@@ -16,6 +22,7 @@ export const Navigation = () => {
             <option value="Europe">Europe</option>
             <option value="Oceania">Oceania</option>
           </select>
+          <ion-icon name="chevron-down-outline"></ion-icon>
         </div>
       </div>
     </nav>
