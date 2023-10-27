00accbe9bf694a66bd6b37b17b2ba3ff3e813322
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-27 12:01:01 +0200

    add markup

--- 20231025_rest_countries/src/components/CountryCard/index.jsx
+++ 20231025_rest_countries/src/components/CountryCard/index.jsx
@@ -1,1 +1,17 @@
 import "./index.scss";
+export const CountryCard = () => {
+  return (
+    <div className="card">
+      <div className="card__image"></div>
+      <p className="card__text">
+        <h3 className="card__text-title">Country Name</h3>
+        <p className="card__text-info">
+          <span className="name">Polulation:</span> 81,770,910
+          <br />
+          <span className="name">Region:</span> Europe <br />
+          <span className="name">Capital:</span> Berlin
+        </p>
+      </p>
+    </div>
+  );
+};
