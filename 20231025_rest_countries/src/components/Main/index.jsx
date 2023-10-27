a5035f12dbdfe048850a10e38dfacdc67ed97c7b
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-27 12:01:29 +0200

    add content

--- 20231025_rest_countries/src/components/Main/index.jsx
+++ 20231025_rest_countries/src/components/Main/index.jsx
@@ -1,9 +1,24 @@
 import "./index.scss";
-
+import { CountryCard } from "../CountryCard";
+import { CountryCardDetail } from "../CountryCardDetail";
 export const Main = () => {
   return (
     <main className="main" role="main">
-      <div className="main__wrapper container-lg"></div>
+      <div className="main__wrapper container-lg center">
+        <section className="section__country">
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+        </section>
+        <section className="section__country-detail">
+          <CountryCardDetail />
+        </section>
+      </div>
     </main>
   );
 };
