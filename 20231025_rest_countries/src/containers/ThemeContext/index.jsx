3f79e9a2c4514e7f36c546c4e789d9024bfe3dcd
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-30 18:50:39 +0100

    refactor loading initial theme

--- 20231025_rest_countries/src/containers/ThemeContext/index.jsx
+++ 20231025_rest_countries/src/containers/ThemeContext/index.jsx
@@ -1,32 +1,31 @@
 /* eslint-disable react/prop-types */
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
+// get initial theme from localStorage or user preference, if none of them is "light" or "dark",
+// dedault theme is light
+const getLocalStorage = () => {
+  let colorMode = localStorage.getItem("colorMode");
+  if (colorMode) {
+    colorMode = JSON.parse(colorMode);
+    if (colorMode === "light" || colorMode === "dark") {
+      return colorMode;
+    }
+  } else {
+    return "";
+  }
+};
+const getThemePreference = () => {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+};
+
+const initialTheme = getLocalStorage() || getThemePreference();
+
 export const ThemeMessage = ({ children }) => {
-  const [theme, setTheme] = useState("");
-  useEffect(() => {
-    // get initial theme from localStorage or user preference, if both are null, set theme to light
-    const getLocalStorage = () => {
-      let colorMode = localStorage.getItem("colorMode");
-      if (colorMode !== "") {
-        colorMode = JSON.parse(colorMode);
-        if (colorMode === "light" || colorMode === "dark") {
-          setTheme(colorMode);
-        } else {
-          getThemePreference();
-        }
-      } else {
-        getThemePreference();
-      }
-    };
-    const getThemePreference = () => {
-      return window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? setTheme("dark")
-        : setTheme("light");
-    };
-    getLocalStorage();
-  }, []);
+  const [theme, setTheme] = useState(initialTheme);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
