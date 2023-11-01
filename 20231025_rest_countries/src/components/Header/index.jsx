d71f73f8122fc806134f9cfb562cd78aaf3c833b
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-01 10:53:37 +0100

    add empty keyword

--- 20231025_rest_countries/src/components/Header/index.jsx
+++ 20231025_rest_countries/src/components/Header/index.jsx
@@ -3,28 +3,28 @@ import { useContext, useEffect } from "react";
 import { ThemeContext } from "../../containers/ThemeContext";
 import { Outlet, Link } from "react-router-dom";
 export const Header = () => {
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  const handleClick = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
+  const { theme, setTheme, setKeyword } = useContext(ThemeContext);
 
   useEffect(() => {
     document.body.className = theme;
     localStorage.setItem("colorMode", JSON.stringify(theme));
   }, [theme]);
 
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <>
       <header className="header" role="banner">
         <div className="header__wrapper container-sm">
           <div className="header-headline">
-            <Link to="/">
+            <Link to="/" onClick={() => setKeyword("")}>
               <h1>Where in the world?</h1>
             </Link>
           </div>
           <div className="theme" aria-label="Color-Toggle">
-            <button onClick={handleClick} className={`theme__toggle ${theme}`}>
+            <button onClick={toggleTheme} className={`theme__toggle ${theme}`}>
               <ion-icon
                 className="theme__toggle__icon"
                 name="moon-outline"
